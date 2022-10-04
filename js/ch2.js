ba531cc16f2e4b128402a9d5005986804cc1f34a
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-04 23:36:46 +0100

    Update ch2.js

--- js/ch2.js
+++ js/ch2.js
@@ -23,3 +23,21 @@ for(i = 0; i < 7; i++){
     hashCharacter = hashCharacter + '#'
     console.log(hashCharacter)
 }
+
+
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
