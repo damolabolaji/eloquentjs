500b880ac8f230dddb4d8ea986ca8d21af307d1e
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-05 17:59:24 +0100

    Update ch3.js
    added minimum exercise solution

--- js/ch3.js
+++ js/ch3.js
@@ -1,1 +1,15 @@
 
+
+function min (a,b){
+    if(a < b){
+    return a
+    }
+      else{
+        return b
+      }
+    }
+    
+    console.log(min(0, 10));
+    // → 0
+    console.log(min(0, -20));
+    // → -20
