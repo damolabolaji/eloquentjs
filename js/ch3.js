fefb7498a0efb1cccc307679ac21f38772544279
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-06 01:33:01 +0100

    Update ch3.js
    added Recursion example

--- js/ch3.js
+++ js/ch3.js
@@ -49,3 +49,15 @@ let width = string.length;
 
 console.log(countChar("BBBabaloC", "B"));
 // → 3
+
+
+//recursion
+function power(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
+
+console.log(power(2, 3));
