5c95fadfb3495a953e453d4d2a4d2b7e8315b47c
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-06 01:34:20 +0100

    Update ch3.js
    added recursion exercise

--- js/ch3.js
+++ js/ch3.js
@@ -61,3 +61,22 @@ function power(base, exponent) {
 }
 
 console.log(power(2, 3));
+
+//recursion exercise
+function isEven(n){
+	if(n == 0){
+		return true;
+	}else if(n == 1){
+		return false;
+	} else if (n < 0) return isEven(-n);
+  	else{
+    return isEven(n - 2);
+    }
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
