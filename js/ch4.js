d44ed3818d6798cd00618aa8c5c0664f58cfc365
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-18 05:01:43 +0100

    Create ch4.js
    added Range exercise

---
+++ js/ch4.js
@@ -0,0 +1,27 @@
+function range(start,end,step){
+    arrayRange =[]
+    step = step || 1
+    if(step > 0){
+        for(let i = start; i <= end; i+=step){
+            arrayRange.push(i)
+        }
+    }
+    else{
+        for(let i = start; i >= end; i+=step){
+            arrayRange.push(i)
+        }
+    }
+    return arrayRange;
+    console.log(arrayRange)
+}
+
+function addRange(array){
+    sum = 0
+    for(let i = 0; i < array.length; i++) {
+        
+        sum+=array[i]}
+        console.log(sum)
+
+}
+
+addRange(range(1,10))
