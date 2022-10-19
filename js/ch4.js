c5cfd6c4a76b260f60dc4dd0ca9432f63c1cb7ef
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-19 23:16:56 +0100

    Update ch4.js

--- js/ch4.js
+++ js/ch4.js
@@ -66,13 +66,17 @@ function arrayReverseInPlace(array){
 arrayReverseInPlace([1,2,3,4,5,6,7])
 
 
-// Your code here.
+function arrayList(array){
+    list =[]
+    for(let i = array.length - 1; i >= 0; i--){
+        
+        list = {
+            value: array[i],
+            rest: list
+        }
+    }
+console.log(list)
+   
+}
 
-console.log(arrayToList([10, 20]));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
+arrayList([1,2,3,4,3,2])
